5e4173e96018d34950344939c136bc7d1a7dd06f
Daniel Rank <drank@jetheaddev.com>
2017-12-22 18:45:05 -0800

    [Ex 2] Add bps_rate module parameter to uart_loop.

--- uart_loop/uart_loop.c
+++ uart_loop/uart_loop.c
@@ -3,9 +3,11 @@
 #include <linux/io.h>
 #include <linux/fs.h>
 
+static long bps_rate = 115200;
+
 static int __init uart_loop_init(void)
 {
-	printk(KERN_EMERG "uart_loop_init\n");
+	printk(KERN_EMERG "uart_loop_init, bps_rate=%ld\n", bps_rate);
 	return 0;
 }
 
@@ -21,4 +23,4 @@ MODULE_VERSION("beta");
 
 module_init(uart_loop_init);
 module_exit(uart_loop_exit);
-
+module_param(bps_rate, long, 0444);
