ed07873163c43965ded882786a44c3f49a632bc0
Daniel Rank <drank@jetheaddev.com>
2017-12-28 18:15:18 -0800

    [Ex 3] Add barrometer_uart2 sysfs loopback attr.

--- uart_loop/uart_loop.c
+++ uart_loop/uart_loop.c
@@ -4,6 +4,38 @@
 #include <linux/fs.h>
 #include <linux/platform_device.h>
 
+/*
+ * Sysfs functions
+ */
+u16 loopback = 0;
+
+static ssize_t loopback_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	printk(KERN_EMERG "loopback_show\n");
+      	// read loopback
+	return sprintf(buf, "%s\n", loopback ? "on" : "off");
+}
+
+static ssize_t loopback_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	u16 value = 0;
+
+	printk(KERN_EMERG "loopback_store: %s\n", buf);
+	
+	if (strcmp(buf, "on\n") == 0)
+	{
+		loopback = 1;
+	}
+	else if (strcmp(buf, "off\n") == 0)
+	{
+		loopback = 0;
+	}
+
+	return count;
+}
+
+static DEVICE_ATTR(loopback, 0644, loopback_show, loopback_store);
+
 /* barrometer_uart2 platform device driver */
 static __devinit int uart_loop_probe(struct platform_device *pdev)
 {
@@ -11,6 +43,12 @@ static __devinit int uart_loop_probe(struct platform_device *pdev)
 
 	printk(KERN_EMERG "uart_loop_probe\n");
 	
+	err = device_create_file(&pdev->dev, &dev_attr_loopback);
+
+	if (err != 0)
+		printk(KERN_WARNING "fb%d: failed to register attributes ()\n",
+				err);
+
 	return err;
 }
 
@@ -20,6 +58,8 @@ static __devexit int uart_loop_remove(struct platform_device *pdev)
 
 	printk(KERN_EMERG "uart_loop_remove\n");
 	
+	device_remove_file(&pdev->dev, &dev_attr_loopback);
+
 	return err;
 }
 
